072017a139f85470703919837ad07060544425fe
giteegrc <53137905+giteegrc@users.noreply.github.com>
2019-07-23 15:52:08 +0800

    c

---
+++ attendance/src/main/java/com/attendance/demo/pojo/Customer.java
@@ -0,0 +1,79 @@
+package com.attendance.demo.pojo;
+
+import java.io.Serializable;
+
+public class Customer implements Serializable {
+
+    private String userName;
+    private String passWord;
+    private String name;
+    private String sex;
+    private String address;
+    private String mobile;
+    private Integer typesid;
+    private Integer deptid;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public Integer getTypesid() {
+        return typesid;
+    }
+
+    public void setTypesid(Integer typesid) {
+        this.typesid = typesid;
+    }
+
+    public Integer getDeptid() {
+        return deptid;
+    }
+
+    public void setDeptid(Integer deptid) {
+        this.deptid = deptid;
+    }
+}
