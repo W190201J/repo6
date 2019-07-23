5ada18b92e83c135ef88c6e6bd7d51b4b25625f8
giteegrc <53137905+giteegrc@users.noreply.github.com>
2019-07-23 14:37:19 +0800

    数据库

---
+++ attendan.sql
@@ -0,0 +1,119 @@
+/*
+SQLyog Ultimate v12.08 (64 bit)
+MySQL - 5.7.18-log : Database - attendance
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`attendance` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `attendance`;
+
+/*Table structure for table `cq` */
+
+DROP TABLE IF EXISTS `cq`;
+
+CREATE TABLE `cq` (
+  `cqid` int(11) NOT NULL,
+  `NAME` varchar(100) DEFAULT NULL,
+  `d1` date DEFAULT NULL,
+  `d2` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`cqid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `cq` */
+
+/*Table structure for table `customer` */
+
+DROP TABLE IF EXISTS `customer`;
+
+CREATE TABLE `customer` (
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `sex` varchar(100) DEFAULT NULL,
+  `address` varchar(100) DEFAULT NULL,
+  `mobile` varchar(100) DEFAULT NULL,
+  `typesid` int(11) DEFAULT NULL,
+  `deptid` int(11) DEFAULT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `customer` */
+
+/*Table structure for table `dept` */
+
+DROP TABLE IF EXISTS `dept`;
+
+CREATE TABLE `dept` (
+  `deptid` int(11) NOT NULL,
+  `deptname` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`deptid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `dept` */
+
+/*Table structure for table `t_admin` */
+
+DROP TABLE IF EXISTS `t_admin`;
+
+CREATE TABLE `t_admin` (
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `t_admin` */
+
+/*Table structure for table `t_contact` */
+
+DROP TABLE IF EXISTS `t_contact`;
+
+CREATE TABLE `t_contact` (
+  `id` int(11) NOT NULL,
+  `customername` varchar(100) DEFAULT NULL,
+  `phone` varchar(100) DEFAULT NULL,
+  `content` varchar(100) DEFAULT NULL,
+  `insertDate` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `t_contact` */
+
+/*Table structure for table `t_jjr` */
+
+DROP TABLE IF EXISTS `t_jjr`;
+
+CREATE TABLE `t_jjr` (
+  `id` int(11) NOT NULL,
+  `v1` date DEFAULT NULL,
+  `v2` varchar(100) DEFAULT NULL,
+  `v3` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `t_jjr` */
+
+/*Table structure for table `types` */
+
+DROP TABLE IF EXISTS `types`;
+
+CREATE TABLE `types` (
+  `typesid` int(11) NOT NULL,
+  `t_types` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`typesid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `types` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
